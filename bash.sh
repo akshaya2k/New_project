1fd5ac3d797cb8ed81c41b1b0c22145da34be81e
akshaya2k <t.a.m.akshaya.23ita@psvpec.in>
2021-05-04 17:01:01 +0530

    shell scripting

---
+++ bash.sh
@@ -0,0 +1,11 @@
+a=$1
+b=$2
+
+c=$(($a+$b))
+echo "addition of 2 nos is $c"
+d=$(($a-$b))
+echo "sub of 2 nos is $d"
+e=$(($a*$b))
+echo "multi of 2 nos is $e"
+f=$(($a/$b))
+echo "div of 2 nos is $f"
